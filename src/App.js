57a7479c61ae962a79f94b31cf07eac1ada36354
suganyasincera <suganya@sincera.in>
2024-10-10 16:43:58 +0530

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,38 +1,24 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import HomePage from './components/screens/HomePage'; 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';// Use BrowserRouter for routing
-import RegisterForm from './components/screens/RegisterForm';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './components/redux/store';
-import ListofProducts from './components/screens/ListofProducts';
-import Cart from './components/screens/Cart';
-import Wishlist from './components/screens/Wishlist';
-import BuyNow from './components/screens/BuyNow';
-import Checkout from './components/screens/Checkout';
-import ProductDetails from './components/screens/ProductDetails';
-import ProceedtoBuy from './components/screens/ProceedtoBuy';
 
 function App() {
   return (
-    <Provider store ={store}>
-        <PersistGate loading={null} persistor={persistor}>
-    <Router> {/* Wrap the Routes with BrowserRouter */}
-      <Routes>
-        <Route exact path="/" element={<HomePage/>} /> 
-        <Route exact path="/RegisterForm" element={<RegisterForm/>} />
-        <Route exact path="/ListofProducts" element={<ListofProducts/>} />
-        <Route exact path="/Cart" element={<Cart/>} />  
-        <Route exact path="/Wishlist" element={<Wishlist/>} /> 
-        <Route exact path="/BuyNow" element={<BuyNow/>} />  
-        <Route exact path="/Checkout" element={<Checkout/>} /> 
-        <Route exact path="/ProductDetails" element={<ProductDetails/>} />  
-        <Route exact path="/ProceedtoBuy" element={<ProceedtoBuy/>} /> {/* Ensure component name is capitalized */}
-      </Routes>
-    </Router>
-    </PersistGate>
-    </Provider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
